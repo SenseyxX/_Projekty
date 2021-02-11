09fb8324496f7079ca5fe846318745f73647b700
SenseyxX <oskgac96@gmail.com>
2021-02-12 00:25:07 +0100

    ASP.net 5

---
+++ MeetupAPI/MeetupAPI/MeetupProfile.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MeetupAPI.Entities;
+using MeetupAPI.Models;
+
+namespace MeetupAPI
+{
+        public class MeetupProfile : Profile
+        {
+            public MeetupProfile()
+            {
+                        CreateMap<Meetup, MeetupDetailsDto>()
+                                .ForMember(m => m.City, map => map.MapFrom(meetup => meetup.Location.City))
+                                .ForMember(m => m.PostCode, map => map.MapFrom(meetup => meetup.Location.PostCode))
+                                .ForMember(m => m.Steet, map => map.MapFrom(meetup => meetup.Location.Steet));
+
+            }
+
+        }
+}
